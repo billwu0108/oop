61b17ae39fb5ad14d37678e9d65be9000930d558
Bill Wu <billwu0108@gmail.com>
2024-05-02 19:58:01 +0800

    Added kshs news fetcher (oop style)

--- get_kshs_news.py
+++ get_kshs_news.py
@@ -2,17 +2,16 @@ import re
 import urllib.request
 
 
-# 把網站的內容撈回來
-def fetch_kshs_web_content():
-    bot = urllib.request.urlopen('https://www.kshs.kh.edu.tw/view/index.php?WebID=269')
-    content = bot.read().decode('utf8')
-    return content
+class KSHS_Bot:
 
-# 用正規表示式把最新消息的標題撈出來
-def fetch_kshs_news(content):
-    for each_news in re.findall(r'28016.+NowSubId=0">(.+)</a>', content):
-        print(each_news)
+    def get_web_content(self):
+        bot = urllib.request.urlopen('https://www.kshs.kh.edu.tw/view/index.php?WebID=269')
+        self.content = bot.read().decode('utf8')
 
+    def show_news(self):
+        for each_news in re.findall(r'28016.+NowSubId=0">(.+)</a>', self.content):
+            print(each_news)
 
-content = fetch_kshs_web_content()
-fetch_kshs_news(content)
+bot = KSHS_Bot()
+bot.get_web_content()
+bot.show_news()
