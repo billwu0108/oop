3b3809e39ec965eca43eff996876eb886e36a0a0
Bill Wu <billwu0108@gmail.com>
2024-05-23 19:50:01 +0800

    Added dunder name

--- calculate_bmi.py
+++ calculate_bmi.py
@@ -11,15 +11,18 @@ class Student:
     def bmi(self):
         return self.weight / (self.height / 100.0) ** 2
 
-    def is_fat():
-        return True if self.bmi >= 35 else False
+    def is_fat(self):
+        return True if self.bmi() >= 35 else False
 
-students = [
-    Student('bill', 169, 100),
-    Student('brian', 164, 56),
-    Student('eagle', 173, 20)
-]
+# 如果我直接被執行的話，才執行以下的程式碼片段
+# 阿如果我不是直接被執行，而是被 import 的話就不要執行
+if __name__ == '__main__':
+    students = [
+        Student('bill', 169, 100),
+        Student('brian', 164, 56),
+        Student('eagle', 173, 20)
+    ]
 
-for student in students:
-    if student.is_fat():
-        print(student.name)
+    for student in students:
+        if student.is_fat():
+            print(student.name)
